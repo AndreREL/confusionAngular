c7f7917128bcecb9b14263f3612c067f37b6bff8
Andre Lino <andrerafael.lino@gmail.com>
2021-11-01 19:46:44 +0000

    HTTP part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -15,6 +15,7 @@ import { Comment } from '../shared/comment';
 export class DishdetailComponent implements OnInit {
 
   dish!: Dish;
+  errMess!: string;
   dishIds!: string[];
   prev!: string;
   next!: string;
@@ -54,7 +55,8 @@ export class DishdetailComponent implements OnInit {
       .subscribe(dish => {
         this.dish = dish;
         this.setPrevNext(dish.id);
-      });
+      },
+      errmess => this.errMess = <any>errmess);
   }
 
   createForm() {
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,6 +10,7 @@ import { DishService } from '../services/dish.service';
 export class MenuComponent implements OnInit {
 
   dishes!: Dish[];
+  errMess!: string;
 
 	constructor(private dishService: DishService,
     @Inject('BaseURL') public BaseURL: any) {
@@ -17,6 +18,7 @@ export class MenuComponent implements OnInit {
 
 	ngOnInit(): void {
     this.dishService.getDishes()
-      .subscribe((dishes) => this.dishes = dishes);
+      .subscribe((dishes) => this.dishes = dishes,
+      errmess => this.errMess = <any>errmess);
 	}
 }
